a3c5374541344eb83fc84379acaaf58871bd4839
24021585-Bùi_Đình_Nguyên <24021585@vnu.edu.vn>
2025-10-17 14:21:28 +0700

    fix method err

--- src/main/java/com/example/arkanoid/ExpandPaddlePowerUp.java
+++ src/main/java/com/example/arkanoid/ExpandPaddlePowerUp.java
@@ -2,4 +2,12 @@ package com.example.arkanoid;
 
 public class ExpandPaddlePowerUp  extends PowerUp {
 
+    public ExpandPaddlePowerUp(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public void applyEffect(Paddle paddle) {
+
+    }
 }
